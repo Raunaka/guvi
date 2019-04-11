75d5526b16e1e99858a2e9705a71d205ec8417cf
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-04-12 00:27:05 +0530

    Create equalaverage.py

---
+++ equalaverage.py
@@ -0,0 +1,22 @@
+import sys, string, math
+def findSubarrays(arr, n):
+    sum = 0;
+    for i in range(0, n):
+        sum =sum + arr[i];
+
+    found = False
+    lsum = 0
+    for i in range(0, n - 1):
+        lsum += arr[i]
+        rsum = sum - lsum
+
+        if lsum * (n - i - 1) == rsum * (i + 1):
+            return True
+    if found == False:
+        return False
+n = int(input())
+L = [ int(x) for x in input().split()]
+if findSubarrays(L, n) : print('yes')
+else :                  
+    print('no')
+

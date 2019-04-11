c3302d2f7a83e823e54651865670052890f65fe3
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-04-11 21:18:55 +0530

    Update ideone_azGsoM.py

--- ideone_azGsoM.py
+++ ideone_azGsoM.py
@@ -1,5 +1,5 @@
 a=input()
 if (a=='Monday'or a=='Tuesday'or a=='Wednesday' or a=='Thrusday'or a=='Friday'):
-    print('yes')
-elif(a=='Saturday'or a=='Sunday'):
     print('no')
+elif(a=='Saturday'or a=='Sunday'):
+    print('yes')

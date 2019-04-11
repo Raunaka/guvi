5bc9f1727002227a569e7d5a9ca8756b6efc5104
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-04-11 23:54:43 +0530

    Add files via upload

---
+++ ideone_Lg4JVU.py
@@ -0,0 +1,12 @@
+m=input()
+s=input()
+m1=[]
+s1=[]
+r=[]
+for i in m:
+   m1.append(ord(i)-ord('a'))
+for i in s:
+   s1.append(ord(i)-ord('a'))
+for i,j in zip(s1,m1):
+   r.append(chr(ord('a')+(i+j)%26+1))
+print("".join(r))

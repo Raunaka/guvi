37176f4aa9a30ffc69b68302a788c4d120dc3bc5
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-05-21 11:48:21 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,8 @@
+a,v=map(int,input().split())
+q,w=map(int,input().split())
+r,e=map(int,input().split())
+t,i=map(int,input().split())
+if(a+q+r+t==v+w+e+i):
+    print("yes")
+else:
+    print("no")

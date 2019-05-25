3db825d276dacbf5cb979db163ba336b8a4d3802
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-05-26 02:00:32 +0530

    Update reverselist.py

--- reverselist.py
+++ reverselist.py
@@ -3,5 +3,6 @@ for i in range(a):
     list1=list(map(int,input().split()))
     q='>-'
     w=q.join(map(str,list1))
-    print(w[::-1],end=" ")
-    print(end=" ")
+    print(w[::-1],end='')
+    break
+    print(end='')

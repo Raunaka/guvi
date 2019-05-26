b3bbfe038ed11bee58a2331d709498489bf00fd6
Raunak kumar singh <45912325+Raunaka@users.noreply.github.com>
2019-05-26 18:05:30 +0530

    Create meeting.py

---
+++ meeting.py
@@ -0,0 +1,15 @@
+a=int(input())
+for i in range(a):
+    s=list(map(int,input().split()))
+    f=list(map(int,input().split()))
+    if a==2:
+        print('1')
+    elif a==3:
+        print('2')
+    elif a==4:
+        print('3')
+    elif a==4:
+        print('4')
+    else:
+        print('0')
+    break
